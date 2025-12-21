78a85054dfcad30bd9d4c5855a7beb2a03787410
citauka <daryaatsitova@icloud.com>
2025-12-21 20:30:12 +0100

    /in page tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -22,7 +22,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     openGraph: {
       title: `Note ${note.title}`,
       description: note.content.slice(0, 30),
-      url: `https://08-zustand-nu-liard.vercel.app${id}`,
+      url: `https://08-zustand-nu-liard.vercel.app/${id}`,
       images: [
         {
           url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
